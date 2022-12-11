60fa46c4c66dbc4cb5efc7a063e79feb26f9f2a8
parthkk <parthkotecha2@gmail.com>
2022-12-10 20:25:41 -0500

    color changes

--- src/shared/Navigation/NavLinks.js
+++ src/shared/Navigation/NavLinks.js
@@ -26,7 +26,7 @@ const NavLinks = (props) => {
       )}
       {!auth.isLoggedIn && (
         <li>
-          <NavLink to="/auth">AUTHENTICATE</NavLink>
+          <NavLink to="/auth">LOGIN/SIGNUP</NavLink>
         </li>
       )}
       {auth.isLoggedIn && (
