0804a2285ef7fafa89a0068e21092f84640c5f03
parthkk <parthkotecha2@gmail.com>
2022-12-17 19:17:48 -0500

    menu bar style changes

--- src/shared/Navigation/NavLinks.js
+++ src/shared/Navigation/NavLinks.js
@@ -3,35 +3,38 @@ import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/auth-context";
 
 import "./NavLinks.css";
+import "./Logout.css";
 
 const NavLinks = (props) => {
   const auth = useContext(AuthContext);
 
   return (
     <ul className="nav-links">
-      <li>
+      <li className="nav-link nav-link-ltr">
         <NavLink end to="/allusers">
           ALL USERS
         </NavLink>
       </li>
       {auth.isLoggedIn && (
-        <li>
+        <li className="nav-link nav-link-ltr">
           <NavLink to={`/${auth.userId}/places`}>MY PLACES</NavLink>
         </li>
       )}
       {auth.isLoggedIn && (
-        <li>
+        <li className="nav-link nav-link-ltr">
           <NavLink to="/places/new">NEW PLACE</NavLink>
         </li>
       )}
       {!auth.isLoggedIn && (
-        <li>
+        <li className="nav-link nav-link-ltr">
           <NavLink to="/auth">LOGIN/SIGNUP</NavLink>
         </li>
       )}
       {auth.isLoggedIn && (
         <li>
-          <button onClick={auth.logout}>LOGOUT</button>
+          <button className="logout" onClick={auth.logout}>
+            LOGOUT
+          </button>
         </li>
       )}
     </ul>
